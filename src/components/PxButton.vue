e7fbaee749aa911c81bfb23eb31b2eb4c043ccb5
Mateo Flores <dragonnegro1231@gmail.com>
2021-07-07 07:45:55 -0500

    Proyecto Listo para salir a internet (version solo ordenadores)

---
+++ src/components/PxButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <button
+    @click="butClick"
+    class="
+      bg-transparent
+      hover:bg-green-500
+      text-green-700
+      font-semibold
+      hover:text-white
+      py-2
+      px-2
+      border border-green-500
+      hover:border-transparent
+      rounded
+    "
+  >
+    <beat-loader :loading="isLoading" :color="'#68d391'" :size="8" />
+    <p v-show="!isLoading">
+      <slot></slot>
+    </p>
+  </button>
+</template>
+<script>
+export default {
+  name: "PxButtom",
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    butClick() {
+      this.$emit("new-click");
+    },
+  },
+};
+</script>
